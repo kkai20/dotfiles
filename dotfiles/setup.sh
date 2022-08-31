e6c044588d46b735e7f65a94c345a4f79f5b05bc
kkai20 <kaneko.umi1020@gmail.com>
2022-09-01 05:21:27 +0900

    fix setup.sh

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -1,8 +1,18 @@
  #!/bin/bash
+# 未定義な変数があったら途中で終了する
+set -u
 
-  DOT_FILES=(.bashrc .gitconfig …)
+# 今のディレクトリ
+# dotfilesディレクトリに移動する
+BASEDIR=$(dirname $0)
+cd $BASEDIR
 
- for file in ${DOT_FILES[@]}
- do
-     ln -s $HOME/dotfiles/$file $HOME/$file
- done
+# dotfilesディレクトリにある、ドットから始まり2文字以上の名前のファイルに対して
+for f in .??*; do
+    [ "$f" = ".git" ] && continue
+    [ "$f" = ".gitconfig.local.template" ] && continue
+    [ "$f" = ".gitmodules" ] && continue
+
+    # シンボリックリンクを貼る
+    ln -snfv ${PWD}/"$f" ~/
+done
